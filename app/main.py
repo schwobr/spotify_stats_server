5ea8b62f54da89f4b746d14f1757cf6c3f226089
Robin Schwob <robin.schwob@student.ecp.fr>
2022-11-07 21:30:47 +0100

    listening to tracks

--- app/main.py
+++ app/main.py
@@ -1,10 +1,18 @@
-from typing import Union
+from time import sleep
 
 import spotipy
-from fastapi import Depends, FastAPI
+from fastapi import BackgroundTasks, Depends, FastAPI
 from fastapi.responses import RedirectResponse
 from spotipy.oauth2 import SpotifyOAuth
 
+
+class Listener:
+    def __init__(self):
+        self.listening = False
+        self.currently_played = None
+
+
+listener = Listener()
 app = FastAPI()
 
 scope = "user-read-playback-state"
@@ -28,9 +36,30 @@ def read_code(code: str = Depends(code_param)):
 
 @app.get("/")
 def read_root():
-    return sp.current_playback()
+    return listener.currently_played
 
 
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+def check_currently_played():
+    while listener.listening:
+        listener.currently_played = sp.current_playback()
+        sleep(1)
+    listener.currently_played = None
+
+
+@app.get("/on")
+def start_listening(background_tasks: BackgroundTasks):
+    listener.listening = True
+    background_tasks.add_task(check_currently_played)
+    return RedirectResponse("/status")
+
+
+@app.get("/off")
+def stop_listening():
+    listener.listening = False
+    return RedirectResponse("/status")
+
+
+@app.get("/status")
+def get_status():
+    status = "on" if listener.listening else "off"
+    return {"status": status}
